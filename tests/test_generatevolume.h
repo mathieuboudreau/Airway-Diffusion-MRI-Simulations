1cbb72751b193a70bf6fc8e7b82d6b9c49521373
Mathieu Boudreau <emb6150@gmail.com>
2017-05-26 12:40:22 -0230

    Fix bug in test

--- tests/test_generatevolume.h
+++ tests/test_generatevolume.h
@@ -11,17 +11,17 @@
 
 START_TEST(test_that_allocvolume_returns_array_with_dimensions_that_match_func_arg)
 {
-    struct Volume * volume;
+    struct Volume volume;
 
-    volume->xdim = 10;
-    volume->ydim = 10;
-    volume->zdim = 10;
+    volume.xdim = 10;
+    volume.ydim = 10;
+    volume.zdim = 10;
 
-    allocvolume(volume);
+    allocvolume(&volume);
 
-    ck_assert(volume->array[9][9][9]==0);
+    ck_assert(volume.array[9][9][9]==0);
 
-    freevolume(volume);
+    freevolume(&volume);
 }
 END_TEST
 
